4f8e12d3f678e4f3f41320a3ff5db634b67fa853
inesmoreno <39106109+inesmoreno@users.noreply.github.com>
2021-02-05 12:36:44 -0500

    Update quant_analysis.R

--- quant_analysis.R
+++ quant_analysis.R
@@ -1,5 +1,6 @@
 d = read.csv('survey_data.tsv', sep='\t')
 
+# Component Analysis
 library(homals)
 names(d[20:(20+11)])
 png('component_analysis.png')
@@ -9,14 +10,16 @@ plot(comp)
 
 dd <- d
 
-# next step: regressions for gold-soci, gold-use/tin-use/gold-env, the rest
+# Next step: regressions for gold-soci, gold-use/tin-use/gold-env, the rest
 dd$PCA1 = as.numeric(factor(d$GOLD.SOCI))
 dd$PCA2 = as.numeric(factor(d$GOLD.USE + d$TIN.USE + d$GOLD.ENV))
 dd$PCA3 = as.numeric(factor(d$TIN.SOCI + d$TIN.ENV + d$TUNGSTEN.SOCI + d$TUNGSTEN.ENV + d$TUNGSTEN.USE + d$COLTAN.USE + d$COLTAN.SOCI +
  d$COLTAN.ENV))
 
-# merge education levels 0 and 1 together since there were only 1 in each
+
 dd$age = factor(d$AGE.GROUP)
+dd$occ = factor(d$OCCUPATION)
+# Merged education levels 0 and 1 together since there were only 1 in each
 dd$educ = d$EDUCATION
 dd$educ[dd$educ==0] <- 1
 dd$educ <- factor(dd$educ)
@@ -46,20 +49,16 @@ dd$organisation = d$Organisations
 dd$conference = d$Conference..technical.papers.reports
 
 
-# regression for gold-soci
+# Regression for gold-soci
 library(MASS)
-f1 = polr(as.factor(PCA1) ~ age + educ + hardware + systems + networks + software + theory + math + info_syst + security + hcc + methodologies + social_prof  + nowhere + survey + news + social + friends + documentaries + church + course + organisation + conference,  Hess=TRUE, method="logistic",data=dd)
-#f1 = lm(PCA1 ~ age + educ + hardware + systems + nowhere + survey)
+f1 = polr(as.factor(PCA1) ~ age + educ + occ + hardware + systems + networks + software + theory + math + info_syst + security + hcc + methodologies + social_prof  + nowhere + survey + news + social + friends + documentaries + church + course + organisation + conference,  Hess=TRUE, method="logistic",data=dd)
 summary(f1)
 
 (ctable <- coef(summary(f1)))
 p <- pnorm(abs(ctable[, "t value"]), lower.tail = FALSE) * 2
 (ctable <- cbind(ctable, "p value" = p))
 
-library(pscl)
-pR2(f1)
-
-f2 = polr(as.factor(PCA2) ~ age + educ + hardware + systems + networks + software + theory + math + info_syst + security + hcc + methodologies + social_prof  + nowhere + survey + news + social + friends + documentaries + church + course + organisation + conference,  Hess=TRUE, method="logistic",data=dd)
+f2 = polr(as.factor(PCA2) ~ age + educ + occ + hardware + systems + networks + software + theory + math + info_syst + security + hcc + methodologies + social_prof  + nowhere + survey + news + social + friends + documentaries + church + course + organisation + conference,  Hess=TRUE, method="logistic",data=dd)
 summary(f2)
 
 (ctable2 <- coef(summary(f2)))
@@ -73,46 +72,39 @@ p <- pnorm(abs(ctable2[, "t value"]), lower.tail = FALSE) * 2
 #p <- pnorm(abs(ctable3[, "t value"]), lower.tail = FALSE) * 2
 #(ctable3 <- cbind(ctable3, "p value" = p))
 
-
+# Polr did not work for PCA3, switched to Bayesian approach
 library(brms)
 
-# look for credible interval doesn't cross 0
-
-f1_brms = brm(PCA1 ~ relevel(age,'5') + relevel(educ,'4') + hardware + systems + networks + software + theory + math + info_syst + security + hcc + methodologies + social_prof  + nowhere + survey + news + social + friends + documentaries + church + course + organisation + conference,
+# Look for credible interval doesn't cross 0
+f1_brms = brm(PCA1 ~ relevel(age,'5') + relevel(educ,'4') + occ + hardware + systems + networks + software + theory + math + info_syst + security + hcc + methodologies + social_prof  + nowhere + survey + news + social + friends + documentaries + church + course + organisation + conference,
   family=cumulative('logit'),data=dd,cores=4,chains=4)
 summary(f1_brms)
 
-f1_brms_mo = brm(PCA1 ~ mo(as.numeric(age)) + mo(as.numeric(educ)) + hardware + systems + networks + software + theory + math + info_syst + security + hcc + methodologies + social_prof  + nowhere + survey + news + social + friends + documentaries + church + course + organisation + conference,
-  family=cumulative('logit'),data=dd,cores=4,chains=4)
-summary(f1_brms_mo)
-
-
-f2_brms = brm(PCA2 ~ relevel(age,'5') + relevel(educ,'4') + hardware + systems + networks + software + theory + math + info_syst + security + hcc + methodologies + social_prof  + nowhere + survey + news + social + friends + documentaries + church + course + organisation + conference,
+f2_brms = brm(PCA2 ~ relevel(age,'5') + relevel(educ,'4') + occ + hardware + systems + networks + software + theory + math + info_syst + security + hcc + methodologies + social_prof  + nowhere + survey + news + social + friends + documentaries + church + course + organisation + conference,
   family=cumulative('logit'),data=dd,cores=4,chains=4)
 summary(f2_brms)
 
-f2_brms_mo = brm(PCA2 ~ mo(as.numeric(age)) + mo(as.numeric(educ)) + hardware + systems + networks + software + theory + math + info_syst + security + hcc + methodologies + social_prof  + nowhere + survey + news + social + friends + documentaries + church + course + organisation + conference,
-  family=cumulative('logit'),data=dd,cores=4,chains=4)
-summary(f2_brms_mo)
-
-
-(ctable2 <- fixef(f2_brms))
-#p <- pnorm(abs(ctable2[, "t value"]), lower.tail = FALSE) * 2
-#(ctable2 <- cbind(ctable2, "p value" = p))
-
-f3_brms = brm(PCA3 ~ relevel(age,'5') + relevel(educ,'4') + hardware + systems + networks + software + theory + math + info_syst + security + hcc + methodologies + social_prof  + nowhere + survey + news + social + friends + documentaries + church + course + organisation + conference,
+f3_brms = brm(PCA3 ~ relevel(age,'5') + relevel(educ,'4') + occ + hardware + systems + networks + software + theory + math + info_syst + security + hcc + methodologies + social_prof  + nowhere + survey + news + social + friends + documentaries + church + course + organisation + conference,
   family=cumulative('logit'),data=dd,cores=4,chains=4)
 summary(f3_brms)
 
 # what about treating age and education as monotonic ordinal variables?
+f1_brms_mo = brm(PCA1 ~ mo(as.numeric(age)) + mo(as.numeric(educ)) + occ + hardware + systems + networks + software + theory + math + info_syst + security + hcc + methodologies + social_prof  + nowhere + survey + news + social + friends + documentaries + church + course + organisation + conference,
+  family=cumulative('logit'),data=dd,cores=4,chains=4)
+summary(f1_brms_mo)
+
+f2_brms_mo = brm(PCA2 ~ mo(as.numeric(age)) + mo(as.numeric(educ)) + occ + hardware + systems + networks + software + theory + math + info_syst + security + hcc + methodologies + social_prof  + nowhere + survey + news + social + friends + documentaries + church + course + organisation + conference,
+  family=cumulative('logit'),data=dd,cores=4,chains=4)
+summary(f2_brms_mo)
+
 f3_brms_mo = brm(PCA3 ~ mo(as.numeric(age)) + mo(as.numeric(educ)) + hardware + systems + networks + software + theory + math + info_syst + security + hcc + methodologies + social_prof  + nowhere + survey + news + social + friends + documentaries + church + course + organisation + conference,
   family=cumulative('logit'),data=dd,cores=4,chains=4)
 summary(f3_brms_mo)
 # the "moas.numericas.characterage" and "moas.numericas.charactereduc" parameters
 # give 'overall' effects of age and education
 
-# plot f1
 
+# Plot results (confidence intervals)
 ciplot <- function(m){
     fe <- fixef(m)
     xlim <- range(fe[,-2])
@@ -137,3 +129,12 @@ ciplot(f2_brms)
 
 png("f3_brms.png")
 ciplot(f3_brms)
+
+png("f1_brms_mo.png")
+ciplot(f1_brms_mo)
+
+png("f2_brms_mo.png")
+ciplot(f2_brms_mo)
+
+png("f3_brms_mo.png")
+ciplot(f3_brms_mo)
